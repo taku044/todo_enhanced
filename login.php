e7a9053889a5518bb65ae9ae182a96b82a19b65d
taku044 <takuitiani0416606@gmail.com>
2025-07-04 10:08:20 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,25 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-   <?php
-   session_start();
-require 'database.php';
-
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->execute([$_POST['username']]);
-    $user = $stmt->fetch();
-
-    if ($user && password_verify($_POST['password'], $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        header("Location: index.php");
-        exit;
-    } else {
-        $error = "ユーザー名またはパスワードが違います";
-    }
-}
-?>
+ 
 <form method="post">
   <input type="text" name="username" placeholder="ユーザー名" required>
   <input type="password" name="password" placeholder="パスワード" required>
